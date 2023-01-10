d989c243221619ebb8a4f89c26a779096b55b9e2
Daniel <118195299+ElNachoMojado@users.noreply.github.com>
2023-01-10 18:58:20 +0000

    Change grid size formula for square size formula that calculates their size so they fit inthe grid box of constant size

--- etchySketchy.js
+++ etchySketchy.js
@@ -1,25 +1,20 @@
 const grid = document.getElementById('grid');
 
 const squaresRow = prompt("How many squares per row would you like? c:");
+const size = (540/squaresRow)-2;
+console.log(size);
 
 function totalSquares () {
     const dimension = squaresRow * squaresRow;
     return dimension;
 }
 
-//Calculates the size in pixels for the grid box so it 
-//accurately places the squares according to user desired
-//grid dimensions 
-function gridWidth () {
-    const pixelWidth = (squaresRow*25)+(2*squaresRow);
-    document.getElementById('grid').style.width = pixelWidth+"px";
-}
-
-gridWidth();
-
-//Places the number of squares stated by the user
+/* Calculates the size of the square so the number of
+squares per row match the quantity given by the user 
+without changing the total size of the grid box. */
 for (i = 1; i <= totalSquares(); i++) {
     const square = document.createElement('div');
     square.classList.add('square');
+    square.setAttribute("style", `height: ${size}px; width: ${size}px`)
     grid.appendChild(square);
 }
