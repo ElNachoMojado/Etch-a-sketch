a309fe919eb8929f608f17398d228f47debd9054
Daniel <118195299+ElNachoMojado@users.noreply.github.com>
2023-01-12 17:00:02 +0000

    Add function to grid size buttons, and color and eraser buttons

--- etchySketchy.js
+++ etchySketchy.js
@@ -1,26 +1,78 @@
-function totalSquares () {
-    const dimension = squaresRow * squaresRow;
-    return dimension;
+//stores color value
+function myColor () {
+    const myColor = document.getElementById('myColor').value;
+    return myColor;
 }
 
-const grid = document.getElementById('grid');
-
-const squaresRow = prompt("How many squares per row would you like? c:");
-const size = (540/squaresRow)-2;
-
-/* Calculates the size of the square so the number of
-squares per row match the quantity given by the user 
-without changing the total size of the grid box. */
-for (i = 1; i <= totalSquares(); i++) {
+//Adds squares to grid box
+function addSquares () {
+    size = (540/squaresRow)-2;
     const square = document.createElement('div');
     square.classList.add('square');
     square.setAttribute("style", `height: ${size}px; width: ${size}px`);
     grid.appendChild(square);
     square.addEventListener("mouseover", () => {
-        square.style.backgroundColor = "black";
+        square.style.backgroundColor = `${color}`;
     });
-   /* square.addEventListener("mouseout", mouseOut);
-    function mouseOut () {
-        square.style.backgroundColor = "white";
-    } */   
 }
+
+//Removes squares from grid box
+function removeSquares () {
+    let square;
+    const totalSquares = grid.getElementsByClassName('square').length;
+    if (totalSquares === 0) {
+
+    } else {
+        for (i = 0; i < totalSquares; i++) {
+            square = document.querySelector('.square');
+            grid.removeChild(square);
+        }
+    }
+}
+
+const grid = document.getElementById('grid');
+let squaresRow;
+let size;
+let color;
+
+//Applies chosen color to paintbrush or erases
+const coloring = document.getElementById('pickColor');
+coloring.addEventListener('click', event => {
+    if (event.target.nodeName == "BUTTON") {
+        if (event.target.textContent == 'Eraser') {
+            color = 'white';
+        } else {
+            color = myColor();
+        }
+    }
+});
+
+//Adds functionality to the many buttons
+const option = document.getElementById('options');
+option.addEventListener('click', event => {
+    if (event.target.nodeName == "BUTTON") {
+        removeSquares();
+        if (event.target.textContent == '8x8') {
+            for (i = 1; i <= 64; i++) {
+                squaresRow = 8;
+                addSquares();
+            }
+        } else if (event.target.textContent == '16x16') {
+            for (i = 1; i <= 256; i++) {
+                squaresRow = 16;
+                addSquares();
+            }
+        } else if (event.target.textContent == '32x32') {
+            for (i = 1; i <= 1024; i++) {
+                squaresRow = 32;
+                addSquares();
+            }
+        } else if (event.target.textContent == '64x64') {
+            for (i = 1; i <= 4096; i++) {
+                squaresRow = 64;
+                addSquares();
+            }
+        } 
+
+    }
+});
